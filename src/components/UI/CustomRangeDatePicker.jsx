b9027375a3da58639a4d72ef27d9b24fed1a2f2c
Moochfer <echinfer@gmail.com>
2023-02-24 08:00:15 -0300

    aa

--- src/components/UI/CustomRangeDatePicker.jsx
+++ src/components/UI/CustomRangeDatePicker.jsx
@@ -23,7 +23,6 @@ const CustomIconButton = styled(IconButton)(({ theme }) => ({
 const CustomDivider = styled(Divider)(({ theme }) => ({
     margin: 0,
 }));
-//Cris branch
 const RangeListItem = ({ name }) => (
     <ListItem disablePadding>
         <ListItemButton>
